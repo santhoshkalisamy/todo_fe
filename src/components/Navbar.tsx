6d6c16485641b196958941506342549ddfff634c
Santhosh K <santhoshkalisamy@gmail.com>
2025-01-11 19:43:29 +0530

    Rename app

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4"
              role="navigation">
             <Logo/>
-            <h2 className="text-2xl font-bold pb-2 text-emerald-700">My Todo Application</h2>
+            <h2 className="text-2xl font-bold pb-2 text-emerald-700">My Awesome Todo Application</h2>
             {/*<TodoSearchBox/>*/}
             {/*<ModeToggle />*/}
         </nav>
